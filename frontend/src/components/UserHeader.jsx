b3473471d93331401fd63d3450d1ff1d614fb7f8
Shehabedin Ahmed Ali <shahabahmed20001@gmail.com>
2025-02-14 21:33:16 +0300

    feat: added a message button in user profiles for easy chat starting.

--- frontend/src/components/UserHeader.jsx
+++ frontend/src/components/UserHeader.jsx
@@ -7,121 +7,159 @@ import { BsInstagram } from "react-icons/bs";
 import { CgMoreO } from "react-icons/cg";
 import { useRecoilValue } from "recoil";
 import userAtom from "../atoms/userAtom";
-import { Link as RouterLink } from "react-router-dom";
+import { Link as RouterLink, useNavigate } from "react-router-dom";
 import useFollowUnfollow from "../hooks/useFollowUnfollow";
+import { selectedConversationAtom } from "../atoms/messagesAtom";
+import { useSetRecoilState } from "recoil";
 
 const UserHeader = ({ user }) => {
-	const toast = useToast();
-	const currentUser = useRecoilValue(userAtom); // logged in user
-	const { handleFollowUnfollow, following, updating } = useFollowUnfollow(user);
+  const toast = useToast();
+  const currentUser = useRecoilValue(userAtom); // logged in user
+  const { handleFollowUnfollow, following, updating } = useFollowUnfollow(user);
+  const setSelectedConversation = useSetRecoilState(selectedConversationAtom);
+  const navigate = useNavigate();
 
-	const copyURL = () => {
-		const currentURL = window.location.href;
-		navigator.clipboard.writeText(currentURL).then(() => {
-			toast({
-				title: "Success.",
-				status: "success",
-				description: "Profile link copied.",
-				duration: 3000,
-				isClosable: true,
-			});
-		});
-	};
+  const copyURL = () => {
+    const currentURL = window.location.href;
+    navigator.clipboard.writeText(currentURL).then(() => {
+      toast({
+        title: "Success.",
+        status: "success",
+        description: "Profile link copied.",
+        duration: 3000,
+        isClosable: true,
+      });
+    });
+  };
 
-	return (
-		<VStack gap={4} alignItems={"start"}>
-			<Flex justifyContent={"space-between"} w={"full"}>
-				<Box>
-					<Text fontSize={"2xl"} fontWeight={"bold"}>
-						{user.name}
-					</Text>
-					<Flex gap={2} alignItems={"center"}>
-						<Text fontSize={"sm"}>{user.username}</Text>
-						<Text fontSize={"xs"} bg={"gray.dark"} color={"gray.light"} p={1} borderRadius={"full"}>
-							threads.net
-						</Text>
-					</Flex>
-				</Box>
-				<Box>
-					{user.profilePic && (
-						<Avatar
-							name={user.name}
-							src={user.profilePic}
-							size={{
-								base: "md",
-								md: "xl",
-							}}
-						/>
-					)}
-					{!user.profilePic && (
-						<Avatar
-							name={user.name}
-							src='https://bit.ly/broken-link'
-							size={{
-								base: "md",
-								md: "xl",
-							}}
-						/>
-					)}
-				</Box>
-			</Flex>
+  const handleMessage = () => {
+    setSelectedConversation({
+      _id: Date.now(),
+      userId: user._id,
+      username: user.username,
+      userProfilePic: user.profilePic,
+      mock: true,
+    });
+    navigate("/chat");
+  };
 
-			<Text>{user.bio}</Text>
+  return (
+    <VStack gap={4} alignItems={"start"}>
+      <Flex justifyContent={"space-between"} w={"full"}>
+        <Box>
+          <Text fontSize={"2xl"} fontWeight={"bold"}>
+            {user.name}
+          </Text>
+          <Flex gap={2} alignItems={"center"}>
+            <Text fontSize={"sm"}>@{user.username}</Text>
+            <Text
+              fontSize={"xs"}
+              bg={"gray.dark"}
+              color={"gray.light"}
+              p={1}
+              borderRadius={"full"}
+            >
+              threads.net
+            </Text>
+          </Flex>
+        </Box>
+        <Box>
+          {user.profilePic && (
+            <Avatar
+              name={user.name}
+              src={user.profilePic}
+              size={{
+                base: "md",
+                md: "xl",
+              }}
+            />
+          )}
+          {!user.profilePic && (
+            <Avatar
+              name={user.name}
+              src="https://bit.ly/broken-link"
+              size={{
+                base: "md",
+                md: "xl",
+              }}
+            />
+          )}
+        </Box>
+      </Flex>
 
-			{currentUser?._id === user._id && (
-				<Link as={RouterLink} to='/update'>
-					<Button size={"sm"}>Update Profile</Button>
-				</Link>
-			)}
-			{currentUser?._id !== user._id && (
-				<Button size={"sm"} onClick={handleFollowUnfollow} isLoading={updating}>
-					{following ? "Unfollow" : "Follow"}
-				</Button>
-			)}
-			<Flex w={"full"} justifyContent={"space-between"}>
-				<Flex gap={2} alignItems={"center"}>
-					<Text color={"gray.light"}>{user.followers.length} followers</Text>
-					<Box w='1' h='1' bg={"gray.light"} borderRadius={"full"}></Box>
-					<Link color={"gray.light"}>instagram.com</Link>
-				</Flex>
-				<Flex>
-					<Box className='icon-container'>
-						<BsInstagram size={24} cursor={"pointer"} />
-					</Box>
-					<Box className='icon-container'>
-						<Menu>
-							<MenuButton>
-								<CgMoreO size={24} cursor={"pointer"} />
-							</MenuButton>
-							<Portal>
-								<MenuList bg={"gray.dark"}>
-									<MenuItem bg={"gray.dark"} onClick={copyURL}>
-										Copy link
-									</MenuItem>
-								</MenuList>
-							</Portal>
-						</Menu>
-					</Box>
-				</Flex>
-			</Flex>
+      <Text>{user.bio}</Text>
 
-			<Flex w={"full"}>
-				<Flex flex={1} borderBottom={"1.5px solid white"} justifyContent={"center"} pb='3' cursor={"pointer"}>
-					<Text fontWeight={"bold"}> Threads</Text>
-				</Flex>
-				<Flex
-					flex={1}
-					borderBottom={"1px solid gray"}
-					justifyContent={"center"}
-					color={"gray.light"}
-					pb='3'
-					cursor={"pointer"}
-				>
-					<Text fontWeight={"bold"}> Replies</Text>
-				</Flex>
-			</Flex>
-		</VStack>
-	);
+      {currentUser?._id === user._id && (
+        <Link as={RouterLink} to="/update">
+          <Button size={"sm"}>Update Profile</Button>
+        </Link>
+      )}
+      {currentUser?._id !== user._id && (
+        <Flex gap={2}>
+          <Button
+            size={"sm"}
+            onClick={handleFollowUnfollow}
+            isLoading={updating}
+          >
+            {following ? "Unfollow" : "Follow"}
+          </Button>
+          {following && (
+            <Button size={"sm"} onClick={handleMessage}>
+              Message
+            </Button>
+          )}
+        </Flex>
+      )}
+      <Flex w={"full"} justifyContent={"space-between"}>
+        <Flex gap={2} alignItems={"center"}>
+          <Text color={"gray.light"}>{user.followers.length} followers</Text>
+          <Box w="1" h="1" bg={"gray.light"} borderRadius={"full"}></Box>
+          <Link color={"gray.light"}>instagram.com</Link>
+        </Flex>
+        <Flex>
+          <Box className="icon-container">
+            <BsInstagram size={24} cursor={"pointer"} />
+          </Box>
+          <Box className="icon-container">
+            <Menu>
+              <MenuButton>
+                <CgMoreO size={24} cursor={"pointer"} />
+              </MenuButton>
+              <Portal>
+                <MenuList bg={"gray.dark"}>
+                  <MenuItem bg={"gray.dark"} onClick={copyURL}>
+                    Copy link
+                  </MenuItem>
+                </MenuList>
+              </Portal>
+            </Menu>
+          </Box>
+        </Flex>
+      </Flex>
+
+      <Flex w={"full"}>
+        <Flex
+          flex={1}
+          borderBottom={"1.5px solid white"}
+          justifyContent={"center"}
+          pb="3"
+          cursor={"pointer"}
+        >
+          <Text fontWeight={"bold"}> Threads</Text>
+        </Flex>
+        <Flex
+          flex={1}
+          borderBottom={"1px solid gray"}
+          justifyContent={"center"}
+          color={"gray.light"}
+          pb="3"
+          cursor={"pointer"}
+        >
+          <Text fontWeight={"bold"}> Replies</Text>
+        </Flex>
+      </Flex>
+    </VStack>
+  );
 };
 
 export default UserHeader;
