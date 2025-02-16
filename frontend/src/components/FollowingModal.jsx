7998e5c2faaccc2de1b6315902bb3d64b4f895e1
Shehabedin Ahmed Ali <shahabahmed20001@gmail.com>
2025-02-16 10:10:44 +0300

    feat: Added followUnfollow feature in the Modal.

--- frontend/src/components/FollowingModal.jsx
+++ frontend/src/components/FollowingModal.jsx
@@ -13,8 +13,13 @@ import {
   Flex,
 } from "@chakra-ui/react";
 import { Link as RouterLink } from "react-router-dom";
+import { useRecoilValue } from "recoil";
+import userAtom from "../atoms/userAtom";
+import useFollowUnfollow from "../hooks/useFollowUnfollow";
 
 const FollowingModal = ({ isOpen, onClose, followings }) => {
+  const currentUser = useRecoilValue(userAtom);
+
   return (
     <Modal isOpen={isOpen} onClose={onClose}>
       <ModalOverlay />
@@ -23,18 +28,49 @@ const FollowingModal = ({ isOpen, onClose, followings }) => {
         <ModalCloseButton />
         <ModalBody>
           <VStack spacing={4} align="start">
-            {followings.map((following) => (
-              <Flex key={following._id} align="center" w="full">
-                <Avatar name={following.username} src={following.profilePic} />
-                <Flex align="start" ml={4} justify="space-between" w="full">
-                  <Text fontWeight="bold">
-                    <RouterLink to={`/${following.username}`} onClick={onClose}>
-                      {following.name} <br />@{following.username}
-                    </RouterLink>
-                  </Text>
+            {followings.map((following) => {
+              const {
+                handleFollowUnfollow,
+                following: isFollowing,
+                updating,
+                // eslint-disable-next-line react-hooks/rules-of-hooks
+              } = useFollowUnfollow(following);
+
+              return (
+                <Flex
+                  key={following._id}
+                  align="center"
+                  w="full"
+                  justify="space-between"
+                >
+                  <Flex align="center">
+                    <Avatar
+                      name={following.username}
+                      src={following.profilePic}
+                    />
+                    <VStack align="start" ml={4}>
+                      <Text fontWeight="bold">
+                        <RouterLink
+                          to={`/${following.username}`}
+                          onClick={onClose}
+                        >
+                          {following.name} <br />@{following.username}
+                        </RouterLink>
+                      </Text>
+                    </VStack>
+                  </Flex>
+                  {currentUser._id !== following._id && (
+                    <Button
+                      size="sm"
+                      onClick={handleFollowUnfollow}
+                      isLoading={updating}
+                    >
+                      {isFollowing ? "Unfollow" : "Follow"}
+                    </Button>
+                  )}
                 </Flex>
-              </Flex>
-            ))}
+              );
+            })}
           </VStack>
         </ModalBody>
         <ModalFooter>
